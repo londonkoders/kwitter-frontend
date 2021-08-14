597d93a297376cf54f35aae86dd3645d6eb16c12
Heath Ryu <32060873+heathryu@users.noreply.github.com>
2021-08-14 16:41:14 +0100

    Add GlobalStyle to normalise and add default font family

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import GlobalStyle from './styles/GlobalStyle';
+import WebFontLoader from 'webfontloader';
 import reportWebVitals from './reportWebVitals';
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto']
+  }
+});
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
