77fa78b7fb2187786dd893fca02338f215a5e697
Jung Lee <26045611+junglee1101@users.noreply.github.com>
2021-08-14 17:38:04 +0100

    Setup React Router and routes (#9)
    Co-authored-by: Jung Lee <jung.lee@lloydsbanking.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import User from './components/User/User';
+import Home from './components/Home/Home';
 
-function App() {
+function App(): React.ReactElement {
   return (
-    <>
-      <h1>Kwitter</h1>
-    </>
+    <Router>
+      <Switch>
+        <Route path="/user">
+          <User />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
